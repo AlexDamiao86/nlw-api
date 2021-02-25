72a4fdca76f66ae999da1f85cc6db482e5931d6f
AlexDamiao86 <alex_maiabr@hotmail.com>
2021-02-25 14:19:46 -0300

    Envia e-mail

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,54 @@
+import { getCustomRepository } from "typeorm";
+import SurveysRepository from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import UsersRepository from "../repositories/UsersRepository";
+import { Request, Response } from 'express';
+import SendMailService from "../services/SendMailService";
+import { resolve } from 'path';
+
+
+class SendMailController {
+  async execute(request: Request, response: Response) {
+    const { email, survey_id } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+    const surveysRepository = getCustomRepository(SurveysRepository);
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const user = await usersRepository.findOne({ email });
+
+    if(!user) {
+      return response.status(400).json({
+        error: "User does not exist",
+      });
+    }
+
+    const survey = await surveysRepository.findOne({ id: survey_id })
+    if(!survey) {
+      return response.status(400).json({
+        error: "Survey does not exist"
+      })
+    }
+
+    const surveyUser = surveysUsersRepository.create({
+      user_id: user.id,
+      survey_id
+    })
+    await surveysUsersRepository.save(surveyUser);
+
+    //Enviar o e-mail para o usuário
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description
+    }
+
+    await SendMailService.execute(email, survey.title, variables, npsPath);
+
+    return response.json(surveyUser);
+  }
+}
+
+export { SendMailController }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,23 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
 import { SurveyController } from '../controllers/SurveyController';
+import { SendMailController } from '../controllers/SendMailController';
 
 const routes = Router();
-
-const userController = new UserController();
-const surveyController = new SurveyController();
-
-routes.post('/users', userController.create);
-routes.post('/surveys', surveyController.create);
 
 routes.get('/health', (_, response) => response.json({
   message: 'UP',
 }));
+
+const userController = new UserController();
+const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
+
+routes.post('/sendMail', sendMailController.execute);
+
 routes.get('/surveys', surveyController.show);
+routes.post('/surveys', surveyController.create);
+
+routes.post('/users', userController.create);
 
 export default routes;
